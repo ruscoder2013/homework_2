b94eb5c7e08b44e7f421724339d150fa1a838636
rusdeveloper23 <gusev-a@mail.ru>
2020-11-17 05:04:53 +0300

    start travis

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ struct map_allocator {
     using value_type = T;
 
     using pointer = T*;
-    using const_pointer = const T*;
+    using const_pointer = const T*; 
     using reference = T&;
     using const_reference = const T&;
 
