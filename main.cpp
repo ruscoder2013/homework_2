11d6666464dd9b0fd8a4aacbb85d9c798e712e70
ruscoder2013 <gusev-a@mail.ru>
2020-12-16 21:03:56 +0200

    new classes

--- main.cpp
+++ main.cpp
@@ -4,44 +4,79 @@
 #include <map>
 #include <memory>
 #include <cstring>
-#include "map_allocator.h"
 
-template<class T, class Alloc = std::allocator<T>>
-class Buffer {
-public:
-    Buffer() {
-    }
-    void emplace_back(T elem) {
-        T* a = alloc.allocate(1);
-        alloc.construct(a, elem);
-        v.emplace_back(a);  
-    }
-    void pop_back() {
-        auto el = v.back();
-        alloc.destroy(el);
-        alloc.deallocate(el, 1);
-        v.pop_back();
-    }
+template<typename T, int N>
+struct map_allocator {
+    using value_type = T;
 
-    typename std::vector<T*>::iterator begin() noexcept
-    {
-        return v.begin();
-    }
+    using pointer = T*;
+    using const_pointer = const T*; 
+    using reference = T&;
+    using const_reference = const T&;
 
-    typename std::vector<T*>::iterator end() noexcept
-    {
-        return v.end();
+    template<typename U>
+    struct rebind {
+        using other = map_allocator<U, N>;
+    };
+
+    map_allocator() {
+        mem_p = nullptr;
+        next_mem = 0;
+        container_size = N;
+        chunk_size = 0;
+        count = 0;
+    }
+    ~map_allocator() {
+        for(const auto& addr: chunck_addrs)
+            std::free(addr);
     }
 
-    void showAll() {
-        for(auto el : v)
+    template<typename U, int M> 
+    map_allocator(const map_allocator<U, M>&) {}
+
+    void* mem_p;
+    int next_mem;
+    int count;
+    int chunk_size;
+    int container_size;
+    std::stack<T*> free_addr;
+    std::vector<void*> chunck_addrs;
+
+    T *allocate(std::size_t n) {
+        void* addr = 0;
+        if (next_mem==container_size)
+            throw std::bad_alloc();
+        if (mem_p == nullptr)
         {
-            std::cout << *el << std::endl;
+            container_size = N;
+            mem_p = std::malloc(N * sizeof(T));
+            if (!mem_p)
+                throw std::bad_alloc();
+            addr = mem_p;
+            next_mem = n;
+        }
+        else {
+            addr = reinterpret_cast<T *>(mem_p) + next_mem;
+            next_mem += n;
+        }
+        return reinterpret_cast<T *>(addr);
+    }
+
+    void deallocate(T *p, std::size_t n) {
+        if (mem_p) {
+            std::free(mem_p);
+            mem_p = nullptr;
         }
     }
-private:
-    std::vector<T*> v;
-    Alloc alloc;
+
+    template<typename U, typename ...Args>
+    void construct(U *p, Args &&...args) {
+        new(p) U(std::forward<Args>(args)...);
+    }
+
+    void destroy(T *p) {
+        p->~T();
+    }
 };
 
 constexpr int fact(int n) {
@@ -52,11 +87,46 @@ constexpr int fact(int n) {
     return res;
 }
 
-int main(int, char *[]) {
-    auto m1 = std::map<int, int>{};
-    for(int i = 0; i < 10; i++)
-        m1[i] = fact(i);  
+template<class T, class Alloc = std::allocator<T>>
+class Buffer {
+public:
+    Buffer(unsigned int size) {
+        data = std::allocator_traits<Alloc>::allocate(alloc, size);
+		next_elem = 0;
+    }
+    
+    ~Buffer()
+    {
+      std::allocator_traits<Alloc>::destroy(alloc, data);
+      std::allocator_traits<Alloc>::deallocate(alloc, data, 1);
+    }
+    
+    void add(T elem) {
+        std::allocator_traits<Alloc>::construct(alloc, &data[next_elem], elem);
+		next_elem++;
+    }
+	
+	void show_all() {
+		for(int i = 0; i < next_elem; i++)
+			std::cout << data[i] << std::endl;
+	}
+	
+	unsigned int get_size() {
+	    return next_elem;
+	}
+	
+	T* at(int n) {
+	    return &data[n];
+	}
+	
+private:
+    typename std::allocator_traits<Alloc>::pointer data;
+    Alloc alloc;
+	int next_elem;
+	int size;
+};
 
+int main() {
     auto m2 = std::map<int,int,std::less<int>,
         map_allocator<std::pair<const int, int>, 10>
      >{};
@@ -65,17 +135,13 @@ int main(int, char *[]) {
         m2[i] = fact(i);
 
     for(auto const &mm : m2)
-        std::cout << mm.first << " " << mm.second << std::endl; 
-
-    Buffer<int> buffer;
-    for(int i = 0; i < 10; i++)
-        buffer.emplace_back(i);
-
-    Buffer<int, map_allocator<int, 10>> buffer_2;
-    for(int i = 0; i < 10; i++)
-        buffer_2.emplace_back(i);
-    for(auto const &buf : buffer_2)
-        std::cout << *buf << std::endl;
-
+        std::cout << mm.first << " " << mm.second << std::endl;
+        
+    Buffer<int, map_allocator<int, 10>> buffer(10);
+    for (int i = 0; i < 10; ++i)
+        buffer.add(fact(i));
+    for(int i = 0; i < buffer.get_size(); i++)
+        std::cout << *buffer.at(i) << std::endl;
+        
     return 0;
 }
